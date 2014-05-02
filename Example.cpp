92f5485fc8ab307960aadae5777ead0d461dcc5f
Michael Grey <mxgrey@gatech.edu>
2014-05-02 13:08:43 -0400

    found violating trajectory

--- Example.cpp
+++ Example.cpp
@@ -41,34 +41,79 @@
 #include <Eigen/Core>
 #include "Trajectory.h"
 #include "Path.h"
+#include <vector>
 
 using namespace std;
 using namespace Eigen;
 
+static void print_limit_violation(const std::string& type, 
+                                  size_t joint_index,
+                                  double limit, double value,
+                                  size_t traj_index)
+{
+    std::cout << "Joint (" << joint_index << ") violated its "
+              << type << " (" << limit << ") with a value of " << value
+              << " at index " << traj_index << "!\n";
+}
+
+const double eps = 1;
+
 int main() {
 	list<VectorXd> waypoints;
-	VectorXd waypoint(3);
-	waypoint << 0.0, 0.0, 0.0;
-	waypoints.push_back(waypoint);
-	waypoint << 0.0, 0.2, 1.0;
-	waypoints.push_back(waypoint);
-	waypoint << 0.0, 3.0, 0.5;
-	waypoints.push_back(waypoint);
-	waypoint << 1.1, 2.0, 0.0;
-	waypoints.push_back(waypoint);
-	waypoint << 1.0, 0.0, 0.0;
-	waypoints.push_back(waypoint);
-	waypoint << 0.0, 1.0, 0.0;
-	waypoints.push_back(waypoint);
-	waypoint << 0.0, 0.0, 1.0;
-	waypoints.push_back(waypoint);
+//	VectorXd waypoint(3);
+//	waypoint << 0.0, 0.0, 0.0;
+//	waypoints.push_back(waypoint);
+//	waypoint << 0.0, 0.2, 1.0;
+//	waypoints.push_back(waypoint);
+//	waypoint << 0.0, 3.0, 0.5;
+//	waypoints.push_back(waypoint);
+//	waypoint << 1.1, 2.0, 0.0;
+//	waypoints.push_back(waypoint);
+//	waypoint << 1.0, 0.0, 0.0;
+//	waypoints.push_back(waypoint);
+//	waypoint << 0.0, 1.0, 0.0;
+//	waypoints.push_back(waypoint);
+//	waypoint << 0.0, 0.0, 1.0;
+    
+    VectorXd waypoint(6);
+    waypoint << -0.603869, -0.21467, 0.361972, -0.81784, 0, 0;
+    waypoints.push_back(waypoint);
+    waypoint << 0, 0, 0, 0, 0, 0;
+    waypoints.push_back(waypoint);
+    waypoint << -0.0503237, -0.0178896, 0.0301651, -0.0681551, 0, 0;
+    waypoints.push_back(waypoint);
+    waypoint << -0.100647, -0.0357792, 0.0603302, -0.13631, 0, 0;
+    waypoints.push_back(waypoint);
+    waypoint << -0.150971, -0.0536689, 0.0904953, -0.204465, 0, 0;
+    waypoints.push_back(waypoint);
+    waypoint << -0.201295, -0.0715585, 0.12066, -0.27262, 0, 0;
+    waypoints.push_back(waypoint);
+    waypoint << -0.251618, -0.0894481, 0.150826, -0.340776, 0, 0;
+    waypoints.push_back(waypoint);
+    waypoint << -0.301942, -0.107338, 0.180991, -0.408931, 0, 0;
+    waypoints.push_back(waypoint);
+    waypoint << -0.352266, -0.125227, 0.211156, -0.477086, 0, 0;
+    waypoints.push_back(waypoint);
+    waypoint << -0.402589, -0.143117, 0.241321, -0.545241, 0, 0;
+    waypoints.push_back(waypoint);
+    waypoint << -0.452913, -0.161007, 0.271486, -0.613396, 0, 0;
+    waypoints.push_back(waypoint);
+    waypoint << -0.503237, -0.178896, 0.301651, -0.681551, 0, 0;
+    waypoints.push_back(waypoint);
+    waypoint << -0.55356, -0.196786, 0.331816, -0.749706, 0, 0;
+    waypoints.push_back(waypoint);
+    waypoint << -0.603884, -0.214675, 0.361981, -0.817861, 0, 0;
+    waypoints.push_back(waypoint);
+    
+    VectorXd ones(waypoint.size()); ones.setOnes();
+    double maxAccel = 0.8;
+	VectorXd maxAcceleration(waypoint.size());
+    maxAcceleration = maxAccel * ones;
+    double maxSpeed = 1.0;
+	VectorXd maxVelocity(waypoint.size());
+    maxVelocity = maxSpeed * ones;
 
-	VectorXd maxAcceleration(3);
-	maxAcceleration << 1.0, 1.0, 1.0;
-	VectorXd maxVelocity(3);
-	maxVelocity << 1.0, 1.0, 1.0;
-
-	Trajectory trajectory(Path(waypoints, 0.1), maxVelocity, maxAcceleration);
+	Trajectory trajectory(Path(waypoints, 0.01), maxVelocity, maxAcceleration);
 	trajectory.outputPhasePlaneTrajectory();
 	if(trajectory.isValid()) {
 		double duration = trajectory.getDuration();
@@ -85,8 +130,71 @@ int main() {
 		cout << "Trajectory generation failed." << endl;
 	}
 
-	string s;
-	cin >> s;
+    double frequency = 200;
+    double dt = 1.0/frequency;
+    size_t traj_count = trajectory.getDuration()*frequency;
+    std::vector<VectorXd> points;
+    for(size_t i=0; i<traj_count; ++i)
+    {
+        points.push_back(trajectory.getPosition(i*dt));
+    }
+    
+    std::cout << "\n" << std::endl;
+    
+    bool limits_okay = true;
+    for(size_t i=0; i<points.size(); ++i)
+    {
+        const VectorXd& elem = points[i];
+        const VectorXd& last_elem = ( i==0 ) ?
+                    points[0] : points[i-1];
+        
+        const VectorXd& next_elem = ( i == points.size()-1 ) ?
+                    points.back() : points[i+1];
+        
+        for(int j=0; j<elem.size(); ++j)
+        {
+
+            if( !(elem[j] == elem[j]) )
+            {
+                print_limit_violation("NaN detection", j,
+                                      0, elem[j], i);
+                limits_okay = false;
+            }
+
+            if( i == 0 )
+            {
+                // No sense in checking speed if this is the first element
+                continue;
+            }
+            
+            double speed = fabs(elem[j] - last_elem[j])
+                           * frequency;
+            if( speed > maxSpeed + eps )
+            {
+                print_limit_violation("max speed", j,
+                                      maxSpeed, speed, i);
+                limits_okay = false;
+            }
+            
+            if( i == 0 || i == points.size()-1 )
+            {
+                // No sense in checking acceleration if this is the first or last element
+                continue;
+            }
+
+            double accel = fabs(next_elem[j] - 2*elem[j] + last_elem[j])
+                           * frequency * frequency;
+            if( accel > maxAccel + eps )
+            {
+                print_limit_violation("max acceleration", j,
+                                      maxAccel, accel, i);
+                limits_okay = false;
+            }
+        }
+    }
+    
+    if(limits_okay)
+        std::cout << "No violations found!" << std::endl;
 }
 
 
